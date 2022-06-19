755b41482292772d5fb424fcdb2e4c8cd0123ed3
saitejakesa <52096261+saitejakesa@users.noreply.github.com>
2022-06-19 08:52:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Card1Display from "./Card1Display";
+import Card2display from "./Card2display";
+import Card3Display from "./Card3Display";
+
+const App = () => {
+  return (
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          <Card1Display></Card1Display>
+          <Card2display></Card2display>
+          <Card3Display></Card3Display>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default App;
